9790512f475a98465a8791842da25e4231174c0c
Kevin Chi <Kevin@Kevins-Air.fios-router.home>
2018-08-10 21:04:54 -0400

    users can only see orders they have made. updated show page for admin

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,8 +2,16 @@ class OrdersController < ApplicationController
 
 
   get '/orders/:id' do
-    @order = Order.find_by_id(params[:id])
-    erb :'orders/show'
+    if logged_in?
+      @order = Order.find_by_id(params[:id])
+      if @order.user == current_user || admin?
+        erb :'orders/show'
+      else
+        redirect '/show'
+      end
+    else
+      redirect '/login'
+    end
   end
 
   get '/orders' do
