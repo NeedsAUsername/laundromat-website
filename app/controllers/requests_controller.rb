d6d47bc7b37d4434071bdf01cfd8b435ea55cf3d
Kevin Chi <Kevin@Kevins-Air.fios-router.home>
2018-08-12 22:12:30 -0400

    adds ability to edit requests

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -29,11 +29,32 @@ class RequestsController < ApplicationController
     end
   end
 
+  get '/requests/:id/edit' do
+    if logged_in?
+      @user_request = Request.find(params[:id])
+      if @user_request.user == current_user
+        erb :'requests/edit'
+      else
+        redirect '/show'
+      end
+    else
+      redirect 'login'
+    end 
+  end
+
+  patch '/requests/:id/patch' do
+    @user_request = Request.find(params[:id])
+    @user_request.update(params[:request])
+    @user_request.save
+    redirect '/show'
+  end
+
   delete '/requests/:id/delete' do
     @user_request = Request.find(params[:id])
     @user_request.delete
 
     redirect '/show'
   end
+
 
 end
